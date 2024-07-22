79784da045e9ffc66bed865e951373d12f2f0069
xhnn <HnnnnnAnq7@163.com>
2024-07-22 19:34:04 +0800

    Site updated: 2024-07-22 19:34:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/17/hello-world/","2024/07/17/my-paly/"];function toRandomPost(){
+var posts=["2024/07/17/hello-world/","2024/07/17/my-paly/","2024/07/22/hello/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
