58b92c10f384ffeff96514cdd755cda15ca442b4
xhnn <HnnnnnAnq7@163.com>
2024-07-22 19:46:38 +0800

    Site updated: 2024-07-22 19:46:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/22/hello-world - 副本/","2024/07/17/hello-world/","2024/07/17/my-paly/"];function toRandomPost(){
+var posts=["2024/07/17/hello-world/","2024/07/17/my-paly/","2024/07/22/hello-world - 副本/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
